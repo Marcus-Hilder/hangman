e7e62a3699a01e21be779eb498ca6037c6379d36
caleb-school <[cp22508@my.westlake.school.nz]>
2024-08-14 14:27:48 +1200

    up dated

---
+++ charater.py
@@ -0,0 +1,11 @@
+words_list = ["machinery", "battery", "ethnic", "bell", "formation", "egg", "cut", "infect", "pleasant", "casualty"]
+
+for i in (words_list): 
+    word = i 
+    individual_characters = list(word)
+    print(individual_characters)
+
+
+
+
+
